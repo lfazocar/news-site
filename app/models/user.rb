1a6d4fa46a4a63ac1ae1bcfbd29ddcbc26dfa5bd
Felipe <lfazocar@users.noreply.github.com>
2023-07-30 19:32:30 -0400

    feat: Added Devise, User model and Annotate gem

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  username               :string
+#  role                   :integer          default("user")
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # 0 = user, 1 = author, 2 = admin
+  enum :role, [:user, :author, :admin]
+
+  # Validations
+  validates :username, uniqueness: true, presence: true
+end
