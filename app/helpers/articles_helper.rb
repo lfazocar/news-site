a4aa4b6d8481b93cfbabbb332383826ee67fff76
Felipe <lfazocar@users.noreply.github.com>
2023-08-07 14:09:32 -0400

    Modified index and show views

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,33 @@
 module ArticlesHelper
+  def time_passed(time)
+    seconds = (Time.now - time).round
+    minutes, seconds = seconds.divmod(60)
+    hours, minutes = minutes.divmod(60)
+    result = ""
+    if hours > 0
+      result += hours.to_s
+      if hours > 1
+        result += " hours "
+      else
+        result += " hour "
+      end
+    end
+    if minutes > 0
+      result += minutes.to_s
+      if minutes > 1
+        result += " minutes "
+      else
+        result += " minute "
+      end
+    end
+    if seconds > 0
+      result += seconds.to_s
+      if seconds > 1
+        result += " seconds "
+      else
+        result += " second "
+      end
+    end
+    result += "ago"
+  end
 end
